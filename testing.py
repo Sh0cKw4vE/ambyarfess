fde39ca9d7a443e6a5a8195e16104145a937c180
Rama <ardigumelar7ramadhan@gmail.com>
2020-02-03 15:47:04 +0700

    initial commit

---
+++ testing.py
@@ -0,0 +1,5 @@
+import re
+
+pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+s = "Mi grupo favorito de CRIMINALISTICA. Ultima clase de cuatrimestre https://t.co/Ad2oWDNd4u"
+print(pattern.sub('', s))
